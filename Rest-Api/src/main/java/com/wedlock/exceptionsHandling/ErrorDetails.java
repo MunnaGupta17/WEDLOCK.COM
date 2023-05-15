bcd4c32a5fec37975a13b02605a177a73667d777
MunnaGupta17 <mgabd17@gmail.com>
2023-05-15 12:07:22 +0530

    GlobalExceptionHandler class created for all exceptions

---
+++ Rest-Api/src/main/java/com/wedlock/exceptionsHandling/ErrorDetails.java
@@ -0,0 +1,53 @@
+package com.wedlock.exceptionsHandling;
+
+import java.time.LocalDateTime;
+
+public class ErrorDetails {
+	
+	private LocalDateTime timeStamp;
+	private String message;
+	private String path;
+	
+	public ErrorDetails() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ErrorDetails(LocalDateTime timeStamp, String message, String path) {
+		super();
+		this.timeStamp = timeStamp;
+		this.message = message;
+		this.path = path;
+	}
+
+	public LocalDateTime getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(LocalDateTime timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	@Override
+	public String toString() {
+		return "ErrorDetails [timeStamp=" + timeStamp + ", message=" + message + ", path=" + path + "]";
+	}
+	
+	
+
+}
---
+++ Rest-Api/src/main/java/com/wedlock/exceptionsHandling/GlobalExceptionHandler.java
@@ -0,0 +1,63 @@
+package com.wedlock.exceptionsHandling;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(UserException.class)
+	public ResponseEntity<ErrorDetails> userException(UserException ue,WebRequest wr){
+		ErrorDetails ed = new ErrorDetails(LocalDateTime.now(), ue.getMessage(), wr.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(ed, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(ReportException.class)
+	public ResponseEntity<ErrorDetails> userException(ReportException re,WebRequest wr){
+		ErrorDetails ed = new ErrorDetails(LocalDateTime.now(), re.getMessage(), wr.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(ed, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(PhotoException.class)
+	public ResponseEntity<ErrorDetails> userException(PhotoException pe,WebRequest wr){
+		ErrorDetails ed = new ErrorDetails(LocalDateTime.now(), pe.getMessage(), wr.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(ed, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(PaymentException.class)
+	public ResponseEntity<ErrorDetails> userException(PaymentException pe,WebRequest wr){
+		ErrorDetails ed = new ErrorDetails(LocalDateTime.now(), pe.getMessage(), wr.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(ed, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(MessageException.class)
+	public ResponseEntity<ErrorDetails> userException(MessageException me,WebRequest wr){
+		ErrorDetails ed = new ErrorDetails(LocalDateTime.now(), me.getMessage(), wr.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(ed, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(MatchException.class)
+	public ResponseEntity<ErrorDetails> userException(MatchException me,WebRequest wr){
+		ErrorDetails ed = new ErrorDetails(LocalDateTime.now(), me.getMessage(), wr.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(ed, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InterestException.class)
+	public ResponseEntity<ErrorDetails> userException(InterestException ie,WebRequest wr){
+		ErrorDetails ed = new ErrorDetails(LocalDateTime.now(), ie.getMessage(), wr.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(ed, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(FeedbackException.class)
+	public ResponseEntity<ErrorDetails> userException(FeedbackException fe,WebRequest wr){
+		ErrorDetails ed = new ErrorDetails(LocalDateTime.now(), fe.getMessage(), wr.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(ed, HttpStatus.BAD_REQUEST);
+	}
+
+}
